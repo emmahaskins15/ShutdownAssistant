954a6d594ee8c4107afea065791e729d19eacfdc
Emma Haskins <emmahaskins15@gmail.com>
2020-02-28 11:03:08 +0100

    Shutdown abort function implemented
    Pressing the "Cancel Shutdown" button will now pass a "shutdown -a"  command to the Windows command line (cmd.exe)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace ShutdownAssistant
 {
@@ -27,8 +28,42 @@ namespace ShutdownAssistant
             timePicker.Value = DateTime.Now;
         }
 
+        //public void TimePicker_UserTimeSelected(object sender, RoutedPropertyChangedEventHandler<DateTime> e)
+        //{
+        //    var picker = sender as TimePicker;
+        //    DateTime? time = picker.Value;
+        //}
+
         public void Shutdown_Click(object sender, RoutedEventArgs e)
         {
+            TimeSpan LocalTime = DateTime.Now.TimeOfDay;
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            process.StartInfo = startInfo;
+            if (timePicker.Value.HasValue)
+            {
+                var UserSelectedTime = timePicker.Value;
+                TimeSpan TimeDifference = (UserSelectedTime - LocalTime).TotalSeconds;
+                startInfo.Arguments = "/C shutdown -s -t " + TimeDifference;
+                System.Windows.MessageBox.Show(startInfo.Arguments);
+                process.Start();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Please enter a valid time", "Error");
+            }
+        }
+        public void Cancel_Shutdown(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            process.StartInfo = startInfo;
+            startInfo.Arguments = "/C shutdown -a";
+            process.Start();
         }
     }
 }
