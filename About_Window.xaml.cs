7275878d8f4904e3aead37d3b4321383e75efb32
Emma Haskins <emmahaskins15@gmail.com>
2020-03-02 11:57:01 +0100

    Fixed code-behind for About_Window.xaml

---
+++ About_Window.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShutdownAssistant
+{
+    /// <summary>
+    /// Interaction logic for About_Window.xaml
+    /// </summary>
+    public partial class About_Window : Window
+    {
+        public About_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
